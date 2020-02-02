a8cdfc391dbe32ea4ac2b40606e8d94629f503cc
mai <mai.yusuf28@yahoo.com>
2020-02-02 15:17:13 +0200

    write the palidrome function

--- challenges/palindrome.js
+++ challenges/palindrome.js
@@ -29,9 +29,47 @@ class LinkedList {
   }
 
   palindrome(list){
+    if (!this.head || !this.head.next){
+      console.log('it has no node or single node ');
+      return false ;
+    }else {
+      let currentNode = this.head ;
+      let arr = [];
+      let i = 0 ;
+      while (currentNode){
+        arr[i] = currentNode ;
+        currentNode = currentNode.next ;
+        i++ ;
+      }
+      // let  arr1 = arr ;
+      let arr2 = arr.slice().reverse() ;
       
+      
+      console.log('arr', arr);
+      console.log('arr2', arr2);
+      let j = 0 ;
+      while ( j < arr.length) {
+        if (arr[j] != arr2[j]){
+          j++;
+          return false;
+
+        }
+        console.log(true);
+        return true;
+      }
+
+    }
+
   }
-
 }
+let test3 = new LinkedList();
 
- module.exports = LinkedList;
+test3.append(3);
+test3.append(2);
+test3.append(5);
+test3.append(6);
+test3.append(3);
+
+// test3.palindrome();
+console.log('result',test3.palindrome() );
+module.exports = LinkedList;
