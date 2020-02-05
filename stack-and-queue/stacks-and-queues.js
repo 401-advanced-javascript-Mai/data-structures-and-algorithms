27f1981b40d6e60b120950a158da57d43509dd7c
mai <mai.yusuf28@yahoo.com>
2020-02-05 03:51:30 +0200

    stack and queue totally finished with test and readme

---
+++ stack-and-queue/stacks-and-queues.js
@@ -0,0 +1,129 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-undef */
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(val) {
+    const node = new Node(val); // create a new node
+    // console.log(node);
+    node.next = this.top;
+    this.top = node; // reassign the stack's top to the new node
+  }
+
+
+  pop() {
+    let top;
+    if (this.top.next) {
+      top = this.top;
+      this.top = this.top.next;
+    } else if (this.top) {
+      top = this.top;
+      this.top = null;
+    } else {
+      return null;
+    }
+    return top.value;  ///// it return the value of pop item
+  }
+
+  peek() {
+    if (this.top) {
+      return this.top.value;
+    }
+    return null;
+  }
+}
+
+////////////////// queue
+
+class Queue {
+  constructor() {
+    this.rare = null;
+    this.front = null;
+  }
+
+
+
+  enqueue(val) {
+
+    const node = new Node(val);
+    node.next = this.rare;
+    this.rare = node;
+    if (!this.front) {
+      this.front = node;
+    }
+  }
+
+  dequeue() {
+    let current = this.rare;
+    // console.log('current', current);
+    if (!current) {
+      return null;
+    }
+
+    //// one item
+    if (!current.next) {
+      this.rare = null;
+      this.front = null;
+      return current.value;
+    }
+
+    while (current.next && current.next.next) {
+      current = current.next;
+    }
+    const target = current.next;
+    // console.log('target' ,target)
+    // console.log('current.next', current.next)
+
+    current.next = null;
+    // console.log('current', current);
+    // console.log('current.next2', current.next)
+    this.front = current;
+    // console.log('this.front', this.front)
+    return target.value;
+  }
+
+  peek() {
+    if (!this.front) {
+      return null;
+    }
+    if (this.front) {
+      return this.front.value;
+    }
+  }
+}
+
+module.exports = { Node, Stack, Queue, } ;
+// const test = new Stack();
+// test.push('a');
+// test.push('b');
+// test.push('c');
+// console.log(test.pop());
+// console.log(test.peek());
+
+
+// ///  test >>>> stack{ top : { value : 'a' , next : node { value :'b' next : node {value : 'c' , next: null }}}
+// console.log ('test', test);
+
+
+// const test2 = new Queue();
+// test2.enqueue('m');
+// test2.enqueue('a');
+// test2.enqueue('i');
+
+
+// ///  test2 >>>> queue{ rare  : { value : 'i' , next : node { value :'a' next : null {value : 'm' , next: null }}}
+// //                     front : node  {value : 'm' , next: null }
+// console.log ('test2', test2);
+// console.log(test2.dequeue());
+// console.log(test2.peek());
---
+++ stack-and-queue/stacks-and-queues.test.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const  { Stack , Queue,} = require('./stacks-and-queues.js');
+
+describe('Stack', () => {
+  it(' push() ..... should push an item', () => {
+    let stack = new Stack();
+    expect(stack.peek()).toBeNull();
+    stack.push('a');
+    expect(stack.top.value).toEqual('a');
+    stack.push('b');
+    expect(stack.top.value).toEqual('b');
+  });
+
+  it(' pop() ..... should return the top item and remove it', () => {
+    let stack = new Stack();
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
+    let item = stack.pop();
+    expect(item).toEqual('c');
+
+  });
+  it('peek() ....... should return the top of stack', () => {
+    let stack = new Stack();
+    stack.push('mai');
+    expect(stack.top.value).toEqual('mai');
+  });
+});
+
+
+
+describe('Queue', () => {
+  it(' enqueue() ..... should add an item', () => {
+    let queue =  new Queue();
+    expect(queue.peek()).toBeNull();
+    queue.enqueue('a');
+    expect(queue.rare.value).toEqual('a');
+    queue.enqueue('b');
+    expect(queue.rare.value).toEqual('b');
+    expect(queue.front.value).toEqual('a');
+
+  });
+
+  it(' dequeue() should return the front item and remove it', () => {
+    let queue =  new Queue();
+    queue.enqueue('a');
+    queue.enqueue('b');
+    queue.enqueue('c');
+    // console.log('queue', queue);
+    let item = queue.dequeue();
+    expect(item).toEqual('a');
+    expect(queue.front.value).toEqual('b');
+
+  });
+  it(' peek() ...... should return the front of queue', () => {
+    let queue =  new Queue();
+    queue.enqueue('mai');
+    expect(queue.front.value).toEqual('mai');
+  });
+});
