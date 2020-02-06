805bd7c8f07a09ceca12c10607ca7993f71adb78
mai <mai.yusuf28@yahoo.com>
2020-02-06 14:15:13 +0200

    fix eslint issue

--- challenges/array-shift/arrayShift.js
+++ challenges/array-shift/arrayShift.js
@@ -1,19 +1,19 @@
-'use strict';
 
-let toBeValid = module.exports = {}
-//////// first way to put the avlue in the middle of array us splice 
+
+let toBeValid = module.exports = {};
+//////// first way to put the avlue in the middle of array us splice
 
 // let array = [1, 2, 3, 4, 5];
 // let value = "mai";
 
 
- toBeValid.insertArray = function(array, value) {
-    let middleIndex = Math.ceil(array.length / 2)
+toBeValid.insertArray = function(array, value) {
+  let middleIndex = Math.ceil(array.length / 2);
 
-    array.splice(middleIndex, 0, value)
-    return array
+  array.splice(middleIndex, 0, value);
+  return array;
 
-}
+};
 // insertArray(array, value);
 
 
@@ -23,26 +23,26 @@ let toBeValid = module.exports = {}
 
 
 
-////////// the second way to add value in the middle of array using for loop 
+////////// the second way to add value in the middle of array using for loop
 let arrayAfterInsert = [];
 toBeValid.insertShiftArray = function(arr, val) {
-    // arr[arr.length+1]= ''
+  // arr[arr.length+1]= ''
 
 
-    for (let index = 0; index <= arr.length; index++) {
-        if (index < Math.ceil(arr.length / 2)) {
-            arrayAfterInsert[index] = arr[index];
-        }
-        else if (index == Math.ceil(arr.length / 2)) {
-            arrayAfterInsert[index] = val;
-        }
-        else if (index > Math.ceil(arr.length / 2)) {
-            arrayAfterInsert[index] = arr[index - 1];
-
-        }
+  for (let index = 0; index <= arr.length; index++) {
+    if (index < Math.ceil(arr.length / 2)) {
+      arrayAfterInsert[index] = arr[index];
+    }
+    else if (index === Math.ceil(arr.length / 2)) {
+      arrayAfterInsert[index] = val;
+    }
+    else if (index > Math.ceil(arr.length / 2)) {
+      arrayAfterInsert[index] = arr[index - 1];
 
     }
-}
+
+  }
+};
 // insertShiftArray([1, 2, 3, 4], "mai");
 console.log(arrayAfterInsert);
 
--- challenges/array-shift/arrayShift.test.js
+++ challenges/array-shift/arrayShift.test.js
@@ -1,21 +1,21 @@
-'use strict';
-const toBeValid = require('../array-shift/arrayShift.js')
+
+const toBeValid = require('../array-shift/arrayShift.js');
 
 
 
 describe('Array shift Module',()=>{
-    it('validate if the function return a new array so it have a new length  ',()=>{
-let array= [ 2,4,6,8] ;
-let value = 5   
-        expect(toBeValid.insertArray(array, value).length).toEqual(5)
-    })
-    it('validate  if the value but in the correct index in the middle of array ',()=>{
-        let testArray=toBeValid.insertArray([2,4,6,8], 5)
-        expect(testArray[2]).toEqual(5)
+  it('validate if the function return a new array so it have a new length  ',()=>{
+    let array = [ 2,4,6,8] ;
+    let value = 5;
+    expect(toBeValid.insertArray(array, value).length).toEqual(5);
+  });
+  it('validate  if the value but in the correct index in the middle of array ',()=>{
+    let testArray = toBeValid.insertArray([2,4,6,8], 5);
+    expect(testArray[2]).toEqual(5);
     // })
-  
-})
-})
+
+  });
+});
 
 
 
@@ -23,13 +23,13 @@ let value = 5
 // describe('Array shift Module',()=>{
 //     it('validate if the function return a new array so it have a new length  ',()=>{
 // let array= [ 2,4,6,8] ;
-// let value = 5   
+// let value = 5
 //         expect(toBeValid.arrayAfterInsert.length).toEqual(5)
 //     })
 //     it('validate  if the value but in the correct index in the middle of array ',()=>{
 //         let testArray=toBeValid.insertShiftArray([2,4,6,8], 5)
 //         expect(testArray[2]).toEqual(5)
 //     // })
-  
+
 // })
 // })
