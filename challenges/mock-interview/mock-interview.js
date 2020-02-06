6dd86e5c3f192105c207e5771e27067510006ec0
mai <mai.yusuf28@yahoo.com>
2020-02-06 14:24:16 +0200

    fix eslint issue

--- challenges/mock-interview/mock-interview.js
+++ challenges/mock-interview/mock-interview.js
@@ -1,41 +1,42 @@
+/* eslint-disable no-undef */
 'use strict';
-let mocks = module.exports = {}
+let mocks = module.exports = {};
 
 
 /////////////////////// first problem //////////////////////////
 mocks.sumRow = function(array){
-  let tempArr=[];
-  console.log(array,tempArr)
-  for(let i=0;i<array.length;i++){
-    let sum =0;
-         for(let j=0;j<array[i].length;j++){
+  let tempArr = [];
+  // console.log(array,tempArr);
+  for(let i = 0;i < array.length;i++){
+    let sum = 0;
+    for(let j = 0;j < array[i].length;j++){
       // console.log('array[j]',array[i][j])
-      sum=sum+array[i][j];
+      sum = sum + array[i][j];
       // console.log('sum', sum)
     }
     tempArr.push(sum);
   }
-return tempArr;
-}
+  return tempArr;
+};
 
-/////////////////////////  ssecond problem ////////////////////// 
+/////////////////////////  ssecond problem //////////////////////
 
 
 let array = [0,1] ;
 mocks.calculate = function(n) {
-for (let i=2 ; i<=n; i++ ){
-  array.push(array[i-1]+ array[i-2]) ;
-// console.log('muna' , muna)
-}
-return array[array.length-2]+array[array.length-3];
-}
+  for (let i = 2 ; i <= n; i++ ){
+    array.push(array[i - 1] + array[i - 2]) ;
+    // console.log('muna' , muna)
+  }
+  return array[array.length - 2] + array[array.length - 3];
+};
 // mocks.calculate(5)
 
 
 mocks.fibonacci = function(num){
-let a = 1 ;
-let b = 0 ; 
-let temp =1 ;
+  let a = 1 ;
+  let b = 0 ;
+  let temp = 1 ;
   while (num >= 1){
     temp = a;
     // console.log(temp);
@@ -46,5 +47,5 @@ let temp =1 ;
   }
 
   return b;
-}
+};
 // mocks.fibonacci(5)
--- challenges/mock-interview/mock-interview.test.js
+++ challenges/mock-interview/mock-interview.test.js
@@ -1,28 +1,29 @@
+/* eslint-disable no-undef */
 'use strict' ;
 
-const mocks = require('./mock-interview.js')
+const mocks = require('./mock-interview.js');
 
 
 describe('matrix sum ' ,()=>{
-    it('sumRow() take array as input make sumassion and return array with apporiprate length ', ()=>{
-   
-   let array =[[1,2,3],[4,5,6],[7,8,9]] ;
-   expect(mocks.sumRow(array)).toEqual([6 ,15 ,24])
-   expect((mocks.sumRow(array)).length).toEqual( 3)
+  it('sumRow() take array as input make sumassion and return array with apporiprate length ', ()=>{
 
-     } ) 
-})
+    let array = [[1,2,3],[4,5,6],[7,8,9]] ;
+    expect(mocks.sumRow(array)).toEqual([6 ,15 ,24]);
+    expect((mocks.sumRow(array)).length).toEqual( 3);
+
+  } );
+});
 
 describe(' fibonacci number ' ,()=>{
-    it('mocks.fibonacci() will take number and give the sum of two sum of the previous and so on   ', ()=>{
-   
-   let value = 5;
-   expect(mocks.fibonacci(value)).toEqual(5)
+  it('mocks.fibonacci() will take number and give the sum of two sum of the previous and so on   ', ()=>{
 
-     } ) 
-     it( 'mocks.calculate () will take number and give the sum of two sum of the previous and so on' , ()=>{
-        let value = 5;
+    let value = 5;
+    expect(mocks.fibonacci(value)).toEqual(5);
 
-         expect(mocks.calculate(value)).toEqual(5)
-     })
-})
+  } );
+  it( 'mocks.calculate () will take number and give the sum of two sum of the previous and so on' , ()=>{
+    let value = 5;
+
+    expect(mocks.calculate(value)).toEqual(5);
+  });
+});
