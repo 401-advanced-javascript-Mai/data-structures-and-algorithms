cd66930c342d57e152892d1cae3de665bff12073
mai <mai.yusuf28@yahoo.com>
2020-03-09 22:58:14 +0200

    awesome solution of breadth first

--- breadth-first/breadth-first.test.js
+++ breadth-first/breadth-first.test.js
@@ -1,30 +1,46 @@
 'use strict ';
 
-const Node = require('./breadth-first.js');
-const BinarySearchTree = require('./breadth-first.js');
-const breadthFirst = require('./breadth-first.js');
+const {Node} = require('./breadth-first.js');
+const {BinaryTree} = require('./breadth-first.js');
+const {levelOrder} = require('./breadth-first.js');
 
 describe('Breadth First()   Module:',()=>{
   it('Can successfully return a collection from a Breadth First traversal',()=>{
-    let one = new Node(1);
-    let two = new Node(27);
-    let three = new Node(7);
-    let five = new Node(8);
-    let six = new Node(4);
-    let seven = new Node(27);
-    let eight = new Node(80);
+    // let one = new Node(10);
+    // let two = new Node(9);
+    // let three = new Node(8);
+    // let five = new Node(7);
+    // let six = new Node(6);
+    // let seven = new Node(5);
+    // let eight = new Node(4);
 
-    let tree = new BinarySearchTree();
-    tree.add(one);
-    tree.add(two);
-    tree.add(three);
-    tree.add(five);
-    tree.add(six);
-    tree.add(seven);
-    tree.add(eight);
-    // console.log(tree.preOrder(),'llllllllllll');
+    // let tree = new BinarySearchTree();
+    // tree.add(one);
+    // tree.add(two);
+    // tree.add(three);
+    // tree.add(five);
+    // tree.add(six);
+    // tree.add(seven);
+    // tree.add(eight);
+    let tree = null;
+
+    let one = new Node('A');
+    let two = new Node('B');
+    let three = new Node('C');
+    let four = new Node('F');
+    let five = new Node('D');
+    let six = new Node ('E');
+  
+    one.left = two;
+    one.right = three;
+    three.left = four;
+    two.left = five;
+    two.right = six ;
+  
+    tree = new BinaryTree(one);
+    // console.log(tree,'llllllllllll');
     //   [ 1, 7, 8, 4, 27, 27, 80 ]   [ 1, 7, 27, 8, 27, 80, 4 ]
-    expect(breadthFirst(tree)).toEqual([ 1, 7, 27, 8, 27, 80, 4 ]);
+    expect(levelOrder(tree)).toEqual([ 1, 7, 27, 8, 27, 80, 4 ]);
 
 
   });
